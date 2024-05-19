04a7a0b32f2fcda32a13306d4453cbafdb65afa9
Farhan Ali <farhanali39765500@gmail.com>
2024-05-19 00:47:22 -0700

    Sub category CRUD applied

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import AdminRoute from './components/routes/AdminRoute'
 import AdminDashboard from './pages/admin/AdminDashboard';
 import CategoryCreate from './pages/admin/category/CategoryCreate'
 import CategoryUpdate from './pages/admin/category/CategoryUpdate';
+import SubCreate from './pages/admin/sub/SubCreate';
+import SubUpdate from './pages/admin/sub/SubUpdate';
 
 
 import { auth } from './firebase';
@@ -67,6 +69,8 @@ const App = () => {
         <AdminRoute exact path='/admin/dashboard' component={AdminDashboard} />
         <AdminRoute exact path='/admin/category' component={CategoryCreate} />
         <AdminRoute exact path='/admin/category/:slug' component={CategoryUpdate} />
+        <AdminRoute exact path='/admin/sub' component={SubCreate} />
+        <AdminRoute exact path='/admin/sub/:slug' component={SubUpdate} />
 
       </Switch>
     </>
