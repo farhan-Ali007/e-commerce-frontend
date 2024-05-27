c9f79fa50918fcd3f411905ac374d6b59704d474
Farhan Ali <farhanali39765500@gmail.com>
2024-05-27 05:08:19 -0700

    Product update form created and working on updating product but not completed yet

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ import SubUpdate from './pages/admin/sub/SubUpdate';
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
 import { currentUser } from './functions/auth'
-import ProductCreate from './pages/product/ProductCreate';
+import ProductCreate from './pages/admin/product/ProductCreate';
+import AllProducts from './pages/admin/product/AllProducts';
+import ProductUpdate from './pages/admin/product/ProductUpdate'
 
 
 const App = () => {
@@ -74,7 +76,9 @@ const App = () => {
         <AdminRoute exact path='/admin/sub' component={SubCreate} />
         <AdminRoute exact path='/admin/sub/:slug' component={SubUpdate} />
         <AdminRoute exact path='/admin/product' component={ProductCreate} />
-    
+        <AdminRoute exact path='/admin/products' component={AllProducts} />
+        <AdminRoute exact path='/admin/product/:slug' component={ProductUpdate} />
+
 
       </Switch>
     </>
